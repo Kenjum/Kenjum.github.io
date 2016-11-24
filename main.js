7d55889953569de9b168627e74157d17d7657b27
Kenji <kenjikiplinger@yahoo.com>
2016-11-24 14:29:27 -0800

    Buttons are better

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-$(document).ready(main);
 
 function main(){
-    var all = true;
-    var drawings = false;
-    var animation = false;
-    var games = false;
+    var ADAG = [true,false,false,false];
 
+    //Default All is on
+    $("#all").addClass("buttonON");
+
+    $(".button").mouseover(function(){
+        $(this).addClass("buttonON");
+    })
+    
+    
+    $(".button").mousedown(function(){
+        $(this).removeClass("buttonON");
+        $(this).addClass("buttonDown");
+    })
+    $(".button").mouseup(function(){
+        $(this).removeClass("buttonDown");
+        $(this).addClass("buttonON");
+    })
+    
+    
+    $(".button").mouseout(function(){
+        $(this).removeClass("buttonDown");
+        if(ADAG[0]!=true){
+            $("#all").removeClass("buttonON");
+        }
+        if(ADAG[1]!=true){
+            $("#drawings").removeClass("buttonON");
+        }        
+        if(ADAG[2]!=true){
+            $("#animation").removeClass("buttonON");
+        }
+        if(ADAG[3]!=true){
+            $("#games").removeClass("buttonON");
+        }        
+    })
 }
 
-
+$(document).ready(main);
